18985cef949517fc8a395d51fda53305f961b211
root <root@ip-172-31-95-254.ec2.internal>
2018-02-24 10:26:11 +0000

    dfaa

---
+++ src/main/java/kiran/in/com/App.java
@@ -0,0 +1,13 @@
+package kiran.in.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
